27b2c19d6feffbdccf266c430c81b6fa89053366
Sumith Yesudasan <ydsumith@users.noreply.github.com>
2020-04-17 22:41:21 -0500

    Update postprocess_contact.py

--- contact-angle-python/postprocess_contact.py
+++ contact-angle-python/postprocess_contact.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import re
 import matplotlib.pyplot as plt
-from scipy.interpolate import Rbf
 import math
 
 def get_current_folder():
@@ -74,40 +73,46 @@ def main():
                 # An expected line should be 2 -47.25 -74.25 0 [type x y z]
                 if len(temp1) == 4:  # for a valid entry
                     if float(temp1[0]) == 1:  # for type 1, water beads
-                        datax = float(temp1[1])
-                        datay = float(temp1[3])
-                        xg2 = math.floor((datax-xlo)/dr)+1
-                        yg2 = math.floor((datay-zlo)/dr)+1
-                        xg1 = xg2 - 1
-                        yg1 = yg2 - 1
-                        xg3 = xg2 + 1
-                        yg3 = yg2 + 1
-                    
-                        xxp = xg2*dr+xlo - datax
-                        w1x = 0.125*(1-4*xxp+4*xxp*xxp)
-                        w2x = 0.25*(3-4*xxp*xxp)
-                        w3x = 0.125*(1+4*xxp+4*xxp*xxp)
+                        try:
+                            datax = float(temp1[1])
+                            datay = float(temp1[3])
+                            xg2 = math.floor((datax-xlo)/dr)+1
+                            yg2 = math.floor((datay-zlo)/dr)+1
+                            xg1 = xg2 - 1
+                            yg1 = yg2 - 1
+                            xg3 = xg2 + 1
+                            yg3 = yg2 + 1
                         
-                        yyp = yg2*dr+zlo - datay
-                        w1y = 0.125*(1-4*yyp+4*yyp*yyp)
-                        w2y = 0.25*(3-4*yyp*yyp)
-                        w3y = 0.125*(1+4*yyp+4*yyp*yyp)
-                        
-                        GZ[xg1][yg1] += w1x*w1y
-                        GZ[xg1][yg2] += w1x*w2y
-                        GZ[xg1][yg3] += w1x*w3y
-                        
-                        GZ[xg2][yg1] += w2x*w1y
-                        GZ[xg2][yg2] += w2x*w2y
-                        GZ[xg2][yg3] += w2x*w3y
-                        
-                        GZ[xg3][yg1] += w3x*w1y
-                        GZ[xg3][yg2] += w3x*w2y
-                        GZ[xg3][yg3] += w3x*w3y
+                            xxp = xg2*dr+xlo - datax
+                            w1x = 0.125*(1-4*xxp+4*xxp*xxp)
+                            w2x = 0.25*(3-4*xxp*xxp)
+                            w3x = 0.125*(1+4*xxp+4*xxp*xxp)
+                            
+                            yyp = yg2*dr+zlo - datay
+                            w1y = 0.125*(1-4*yyp+4*yyp*yyp)
+                            w2y = 0.25*(3-4*yyp*yyp)
+                            w3y = 0.125*(1+4*yyp+4*yyp*yyp)
+                            
+                            GZ[yg1][xg1] += w1x*w1y
+                            GZ[yg2][xg1] += w1x*w2y
+                            GZ[yg3][xg1] += w1x*w3y
+                            
+                            GZ[yg1][xg2] += w2x*w1y
+                            GZ[yg2][xg2] += w2x*w2y
+                            GZ[yg3][xg2] += w2x*w3y
+                            
+                            GZ[yg1][xg3] += w3x*w1y
+                            GZ[yg2][xg3] += w3x*w2y
+                            GZ[yg3][xg3] += w3x*w3y
+                        except IndexError:
+                            print("xi = %.2f, yi = %.2f, xg2 = %d, yg2 = %d" % (datax,datay,xg2,yg2) )
+                        except:
+                            print("Something else went wrong")
+                            
 
         # ---plotting business
         plt.show()
-        plt.contourf(GX, GY, GZ, 20, cmap='binary')
+        plt.contourf(GX, GY, GZ, 20, cmap='YlGnBu')
         plt.colorbar()
         plt.axis(aspect='image')
         plt.savefig(out_image, dpi=1200)
